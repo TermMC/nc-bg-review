3262112c8c69d38208b108c8b35db20bd0d5fd3e
TermMC <termmc@gmail.com>
2021-12-02 12:56:28 +0000

    happy path complete for /api endpoint

--- routers/api.router.js
+++ routers/api.router.js
@@ -2,11 +2,13 @@ const express = require("express");
 const commentsRouter = require("./comments.router");
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
+const { getAPI } = require("../controllers/api.controller");
 
 const apiRouter = express.Router();
+
 
 apiRouter.use("/categories", categoriesRouter);
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
-
+apiRouter.route('/').get(getAPI)
 module.exports = apiRouter;
